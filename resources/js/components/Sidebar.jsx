3282a05420f815fbc6fb7ba90155233bbb7c858c
Yogi Adam Firdaus <yogiadamf@gmail.com>
2022-12-25 19:01:39 +0700

    add auth controller

--- resources/js/components/Sidebar.jsx
+++ resources/js/components/Sidebar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Sidebar() {
+    const [open,setOpen] = useState(true);
+
     return (
         <div className="flex">
-            <div className="bg-dark-purple h-screen p-5 pt-8 w-72 text-white">
-            <FontAwesomeIcon icon={faBars} className="cursor-pointer"/>
+            <div className={`bg-dark-purple h-screen p-5 pt-8 ${open ?"w-72" : "w-20"} text-white`}>
+            <FontAwesomeIcon icon={faBars} className="cursor-pointer pr-2" onClick={()=>setOpen(!open)}/>
             </div>
             <div className="p-7">
                 <h1 className="text-2xl font-semibold">Home Page</h1>
